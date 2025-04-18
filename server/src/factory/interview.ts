5f186a9f5dce44d1296585aa12519e8155fdeb94
dmztDhruv <=>
2025-04-19 01:30:33 +0530

    Removed redundant function to create interview questions

--- server/src/factory/interview.ts
+++ server/src/factory/interview.ts
@@ -6,43 +6,6 @@ import { getJobInterviewService } from "../services/inteview";
 
 const factory = createFactory();
 
-// export const createInterviewQuestionForJobHandler = factory.createHandlers(
-//    createJobInterviewQuestionsCreation,
-//    authenticatedAuthToken,
-//    async (c) => {
-//       try {
-//          const user = c.get("auth_user");
-//          const jobPostId = c.req.param("id");
-//
-//          if (!jobPostId) {
-//             throw new ZentioError("job post id is not provided", 400);
-//          }
-//
-//          const res = await generateJobPostQuestionsById(user.id, jobPostId);
-//          return successResponse({
-//             c,
-//             message: "successfully generated questions",
-//             data: res,
-//             statusCode: 200,
-//          });
-//       } catch (error) {
-//          console.log(`Error fetching job post`, error);
-//          if (error instanceof ZentioError) {
-//             return errorResponse({
-//                c,
-//                statusCode: error.status,
-//                message: error.message,
-//             });
-//          }
-//          return errorResponse({
-//             c,
-//             statusCode: 500,
-//             message: "internal server error",
-//          });
-//       }
-//    },
-// );
-
 export const getJobInterviewHandler = factory.createHandlers(
    authenticatedAuthToken,
    async (c) => {
